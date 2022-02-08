b9e3561cee04b5c3a64066bdad9a2565e78f990c
SindhujaObulasetti <sindhujaobulasetti17@gmail.com>
2022-02-08 15:16:14 +0530

    initialcommit

---
+++ Entity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    //TASK1
+    public class Entity 
+    {
+
+        public class Product
+        {
+            public string ProductId { get; set; }
+            public string ProductName { get; set; }
+            public string Description { get; set; }
+            public double Cost { get; set; }
+            public int Quantity { get; set; }
+            public string ProductSoldDate { get; set; }
+            public string DisplayProductId(string productid)
+            {
+                ProductId = productid;
+                return ProductId;
+
+                
+            }
+            public string DisplayProductName(string productname)
+            {
+                ProductName= productname;
+                return ProductName;
+            }
+            public string DisplayDescription(string description)
+            {
+                Description = description;
+                return Description;
+            }
+            public string DisplayProductSoldDate(string solddate)
+            {
+                ProductSoldDate = solddate;
+                return ProductSoldDate;
+            }
+            public double DisplayCost(double cost)
+            {
+                Cost= cost;
+                return Cost;
+            }
+            public int DisplayQuantity(int quantity)
+            {
+                Quantity = quantity*(int)Cost;  
+                return Quantity;
+            }
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //TASK 2
+            string ProductId;
+            string ProductName;
+            string Description;
+            string ProductSoldDate;
+            double Cost;
+            int Quantity;
+            Entity.Product product1=new Entity.Product();
+            Console.WriteLine("Enter ProductId: ");
+            ProductId=Console.ReadLine();
+
+            Console.WriteLine("Enter ProductName: ");
+            ProductName=Console.ReadLine();
+
+            Console.WriteLine("Enter Description: ");
+            Description=Console.ReadLine();
+
+            Console.WriteLine("Enter ProductSoldDate: ");
+            ProductSoldDate=Console.ReadLine();
+
+            Console.WriteLine("Enter Cost: ");
+            Cost=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Quantity");
+            Quantity=Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            
+            Console.WriteLine("ProductId: "+ product1.DisplayProductId(ProductId));
+            Console.WriteLine("Product Name: "+ product1.DisplayProductName(ProductName));
+            Console.WriteLine("Product Description: " + product1.DisplayDescription(Description));
+            Console.WriteLine("Product Sold Date: " + product1.DisplayProductSoldDate(ProductSoldDate));
+            Console.WriteLine("Product Cost: " + product1.DisplayCost(Cost));
+            Console.WriteLine("Product Quantity: " + product1.DisplayQuantity(Quantity));
+
+
+        }
+    }
+}
